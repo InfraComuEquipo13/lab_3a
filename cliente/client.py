2ae8f51ca9709594ad9a7cf720ac323a2e7a1e3e
Juan Sebastian Pinzon <Js.pinzonr@uniandes.edu.co>
2022-10-02 18:33:26 -0500

    Funcional
    a

--- cliente/client.py
+++ cliente/client.py
@@ -1,64 +1,196 @@
 # Echo client program
+
+# importing the hashlib module
+import hashlib
+from asyncio import Queue
 import socket
 import struct
 import threading
+import time
+import logging
+import datetime
+import os
 
+barrier = None
+n_conn = int(input('Ingrese el número de conexiones: '))
+tipo_archivo = int(input('Ingrese "0" si desea recibir un archivo de 100MB y "1" si desea uno de 250MB: '))
+#n_conn = 2
+#tipo_archivo = "1"
 
-def receive_file_size(sck: socket.socket):
+# Creación del logger
+## Fecha de la prueba
+now = datetime.datetime.now()
+#os.mkdir('Logs')
+
+logging.basicConfig(filename=f'Logs\prueba{n_conn}-{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}-log.txt', 
+                             encoding='utf-8',
+                             format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+                             level=logging.INFO,
+                             datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger("client_logger")
+
+class ThreadedClient(threading.Thread):
+    ### CONSTRUCTOR DE LA CLASE 
     
-    fmt = "<Q"
-    expected_bytes = struct.calcsize(fmt)
-    received_bytes = 0
-    stream = bytes()
-    while received_bytes < expected_bytes:
-        chunk = sck.recv(expected_bytes - received_bytes)
-        stream += chunk
-        received_bytes += len(chunk)
-    filesize = struct.unpack(fmt, stream)[0]
-    return filesize
+    def __init__(self,  numcliente, n_conn ,host, port, tipo_archivo, logger):
+        threading.Thread.__init__(self)
+        #set up queues
+        self.send_q = Queue(maxsize = 10)
+        #declare instance variables       
+        self.numcliente= numcliente
+        self.host = host
+        self.port = port
+        self.n_conn = n_conn
+        self.tipo_archivo = tipo_archivo
+        #connect to socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logger.info(f' Cliente #{self.numcliente}: Estableciendo conexión con el servidor.') 
+        self.s.connect((self.host, self.port))
+        logger.info(f' Cliente #{self.numcliente}: Conexión con el servidor exitosa en el puerto {self.port}.') 
 
-def receive_file(sck: socket.socket, filename):
-    # Leer primero del socket la cantidad de bytes que se recibirán del archivo.
 
-    filesize = receive_file_size(sck)
-    # Abrir un nuevo archivo en donde guardar los datos recibidos.
-    with open(filename, "wb") as f:
+    def run(self):
+        #print(str(self.numcliente) + "\n")
+        logger.info(f' Cliente #{self.numcliente}: A la espera de que los demás clientes se conecten. Restan {barrier.n_waiting}') 
+        barrier.wait()
+
+        ################################################
+    def start_receive_file(self):
+        self.filepath = f"ArchivosRecibidos\Cliente{self.numcliente}-Prueba-{self.n_conn}.txt"
+        th = threading.Thread(target=self.receive_file, args=(self.s, self.filepath,))
+        print(f"Recibiendo archivo en el cliente{self.numcliente}...")
+        th.start()
+        print(f"Archivo recibido en el cliente{self.numcliente}.")
+        #t.join()  
+
+    def receive_file_size(self, sck: socket.socket):
+        
+        fmt = "<Q"
+        expected_bytes = struct.calcsize(fmt)
         received_bytes = 0
-        # Recibir los datos del archivo en bloques de 1024 bytes hasta llegar
-        # a la cantidad de bytes total informada por el cliente.
-        while received_bytes < filesize:
-            chunk = sck.recv(1024)
-            if chunk:
-                f.write(chunk)
-                received_bytes += len(chunk)
+        stream = bytes()
+        while received_bytes < expected_bytes:
+            chunk = sck.recv(expected_bytes - received_bytes)
+            stream += chunk
+            received_bytes += len(chunk)
+        filesize = struct.unpack(fmt, stream)[0]
+        
+        logger.info(f' Cliente #{self.numcliente}: Se recibió el tamaño real ({filesize}) del archivo que se recibirá.') 
+        return filesize
+
+    def receive_file(self, sck: socket.socket, filename):
+
+        
+        ### Envía el tipo de archivo que recibirá
+        sck.sendall("{0}".format(tipo_archivo).encode())  
+        logger.info(f' Cliente #{self.numcliente}: Se le envió al servidor el tipo de archivo que se desea recibir.')    
+        
+        
+        #filename_server = sck.recv(1024).decode("utf-8")
+        filename_server = "hoa"
+        logger.info(f' Cliente #{self.numcliente}: Se recibió nombre del archivo que se recibirá es {filename_server}.') 
+        
+        logger.info(f' Cliente #{self.numcliente}: Se recibió el mensaje de solicitud de confirmación.') 
+        #sck.sendall("{0}".format("0").encode())  
+        
+        ### Recibe el mensaje de solicitud de confirmación del servidor
+        sck.recv(1024)
+        
+        ### Enviar la confirmación de listo
+        sck.sendall("{0}".format("1").encode())    
+        logger.info(f' Cliente #{self.numcliente}: Enviada la confirmación de listo para iniciar la transferencia.') 
+        ### Leer primero del socket la cantidad de bytes que se recibirán del archivo.
+        filesize = self.receive_file_size(self.s)
+        # Abrir un nuevo archivo en donde guardar los datos recibidos.
+        
+        logger.info(f' Cliente #{self.numcliente}: Inicio de la transferencia del archivo.') 
+        try:
+            with open(filename, "wb") as f:
+                received_bytes = 0
+                # Recibir los datos del archivo en bloques de 1024 bytes hasta llegar
+                # a la cantidad de bytes total informada por el cliente.
+                
+                time_start = time.time()
+                while received_bytes < filesize:
+                    chunk = sck.recv(1024)
+                    if chunk:
+                        f.write(chunk)
+                        received_bytes += len(chunk)
+                time_end = round(time.time()-time_start, 5)
+            logger.info(f' Cliente #{self.numcliente}: El archivo fue recibido con exito. El tiempo de transferencia fue: {time_end} segundos.') 
+        except:
+            logger.info(f' Cliente #{self.numcliente}: La transferencia no fue exitosa.') 
+        
+        ## Integridad del archivo
+        
+        ### Recibe el hashcode calculado por el servidor.
+        
+        hashcode_server = sck.recv(1024).decode("utf-8")
+        logger.info(f' Cliente #{self.numcliente}: Se recibió el codigo hash del archivo calculado por el servidor. Hashcode: "{hashcode_server}".') 
+
+        ### Hashcode calculado por el cliente.
+        hashcode_cliente = self.hash_file(self.filepath)
+        logger.info(f' Cliente #{self.numcliente}: Se calculó el codigo hash del archivo recibido. Hashcode: "{hashcode_cliente}".') 
+        
+        if(hashcode_server == hashcode_cliente ):
+            logger.info(f' Cliente #{self.numcliente}: La integridad del archivo se conservó, los hashcode entre servidor y cliente coinciden.')
+        
+        #barrier.wait()
+    
+    def hash_file(self, filename):
+       """"This function returns the SHA-1 hash
+       of the file passed into it"""
+    
+       # make a hash object
+       h = hashlib.sha1()
+    
+       # open file for reading in binary mode
+       with open(filename,'rb') as file:
+    
+           # loop till the end of the file
+           chunk = 0
+           while chunk != b'':
+               # read only 1024 bytes at a time
+               chunk = file.read(1024)
+               h.update(chunk)
+    
+       # return the hex representation of digest
+       return h.hexdigest()
 
 
-# Get local machine name
-HOST = socket.gethostname() 
-# The same port as used by the server
-PORT = 50000              
+if __name__ == '__main__':
+
+    #We need one party for each thread we intend to create, five in this place, 
+    #as well as an additional party for the main thread that will also wait for all threads to reach the barrier.
+    barrier = threading.Barrier(n_conn+1 )
+    
+    # Get local machine name
+    HOST = socket.gethostname() 
+    PORT = 50000       
+    logger.info(f' AppCliente: Iniciando la aplicación de clientes con el servidor {HOST} en el puerto {PORT}.') 
+    
+    i=1
+    clientes_list = []
+    while i <= n_conn:   
+        print("Iniciando la conexión del cliente", i)
+        logger.info(f' AppCliente: Iniciando la conexión del cliente {i}.') 
+        s = ThreadedClient(i, n_conn, HOST, PORT, tipo_archivo, logging)
+        s.start()
+        clientes_list.append(s)
+        i+=1
+    print("Todos las conexiones fueron exitosas, listos para iniciar la recepción del archivo.")
+    barrier.wait() ## barrier + 1 
+
+    #barrier = threading.Barrier(n_conn+1 )
+    for s in clientes_list:
+        
+        s.start_receive_file()
+    #barrier.wait()
+    #logger = None
 
 #s.sendall(bytes('Hello, world', 'utf-8'))
 #data = s.recv(1024)
 
-n_conn=5
-#receive_file(s, f"Cliente{i}-Prueba-{n_conn}.txt")
-
-i=1
-while i <= n_conn:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-
-    th = threading.Thread(target=receive_file, args=(s, f"Cliente{i}-Prueba-{n_conn}.txt",))
-    
-    print(f"Recibiendo archivo en el cliente{i}...")
-    th.start()    
-    print(f"Archivo recibido en el cliente{i}.")
-    th.join()
-    #Note it's (addr,) not (addr) because second parameter is a tuple
-    #Edit: (c,addr)
-    #that's how you pass arguments to functions when creating new threads using thread module.
-    i+=1
-s.close()
 
 #print ('Received', repr(data))
--- server/server.py
+++ server/server.py
@@ -1,44 +1,118 @@
 #!/usr/bin/python           # This is server.py file                                                                                                                                                                           
 
-
+# importing the hashlib module
+import hashlib
 import os
 import socket
 import struct
 import threading
+import logging
+import datetime
+import time
+
+now = datetime.datetime.now()
+## Creación del logger_s
+#logger_s = logging.basicConfig(filename=f'{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}-log.txt', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename=f'{now.year}-{now.month}-{now.day}-{now.hour}-{now.minute}-{now.second}-log.txt', 
+                             encoding='utf-8',
+                             format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+                             level=logging.INFO,
+                             datefmt='%Y-%m-%d %H:%M:%S')
+#logging.basicConfig(filename='prueba.txt', level=logging.DEBUG)
+logger_s = logging.getLogger("server_logger")
 
 fileprueba = "prueba.tiff"
-filename_1 = "archivo_100mb.zip"
+filename_0 = "archivo_100mb.zip"
 
-filename_2 = "archivo_250mb.zip"
-
+filename_1 = "archivo_250mb.zip"
+filesize = 0
 def send_file(sck: socket.socket, filename):
     # Obtener el tamaño del archivo a enviar.
     filesize = os.path.getsize(filename)
     # Informar primero al servidor la cantidad
     # de bytes que serán enviados.
     sck.sendall(struct.pack("<Q", filesize))
+    time_start = time.time()
     # Enviar el archivo en bloques de 1024 bytes.
     with open(filename, "rb") as f:
         while read_bytes := f.read(1024):
             sck.sendall(read_bytes)
+    return time.time() - time_start
 
-
-def on_new_client(clientsocket,addr):
+def on_new_client(clientsocket,addr, num_cliente):
     while True:
+        
+        logger_s.info(f' Server-Cliente #{num_cliente}:' + 
+                     'Inicio del manejador de la conexión, la dirección asociada es {addr}.')
         #msg = clientsocket.recv(1024)
+        
+   
         #do some checks and if msg == someWeirdSignal: break:
-      
-        print (addr, ' >> ')
+        tipo_archivo = clientsocket.recv(1024).decode("utf-8")
+        
+        filename = filename_0 if str(tipo_archivo ) == '0' else filename_1
+        
+        logger_s.info(f' Server-Cliente #{num_cliente}: ' 
+                     +  f'El tipo de archivo solicitado por el cliente es {filename} ({tipo_archivo}) cuyo tamaño es de {os.path.getsize(filename)} .')
+        
+        
+             
+        logger_s.info(f' Server-Cliente #{num_cliente}:' + 
+                     'Enviando el nombre del archivo al cliente.')
+        #clientsocket.sendall("{0}".format(filename).encode())     
+        
+        #print (addr, ' >>  Tipo de archivo: ', tipo_archivo)
         #msg = 'SERVER >> '
         #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
         #clientsocket.send(bytes(msg, 'utf-8'))
         #break
+        #clientsocket.recv(1024)
+        clientsocket.sendall(b"Envie un 1 si se encuentra listo para la recepcion del archivo")
+           
+        logger_s.info(f' Server-Cliente #{num_cliente}: Se le envió la solicitud de confirmación al cliente. En espera.')
+       #print(" #{0} \n".format(num_cliente))
+        confirmacion = clientsocket.recv(1024).decode("utf-8")
+        if confirmacion == '1': 
+            logger_s.info(f' Server-Cliente #{num_cliente}: El cliente ha confirmado que esta listo para recibir el archivo.')  
+  
         
+
+        logger_s.info(f' Server-Cliente #{num_cliente}: Enviando archivo...')  
+        try:
+            tiempo_envio = send_file(clientsocket, filename)
+            tiempo_envio = round(tiempo_envio, 5)
+            logger_s.info(f' Server-Cliente #{num_cliente}: Envio finalizado con exito. El tiempo de transferencia fue de {tiempo_envio} segundos.') 
+        except:
+            logger_s.info(f' Server-Cliente #{num_cliente}: Envio finalizado sin exito.') 
+        ## Envío del hashcode
+        hash_code = hash_file(filename)
+        clientsocket.sendall(hash_code.encode())
+        logger_s.info(f' Server-Cliente #{num_cliente}: El hash del archivo enviado es {hash_code}. Se envió el codigo hash calculado al cliente.')
         
-        print("Enviando archivo...")
-        send_file(clientsocket, filename_1)
-        print("Enviado.")
+      
     clientsocket.close()
+
+
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+
+   # make a hash object
+   h = hashlib.sha1()
+
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return the hex representation of digest
+   return h.hexdigest()
+
 
 
 
@@ -46,19 +120,23 @@ def on_new_client(clientsocket,addr):
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 50000                # Reserve a port for your service.
+logger_s.info(f' Server: El servidor {host} en el puerto {port} ha sido inicializado.')
+logger_s.info(' Server: Esperando por clientes.')
 
-print ('Server started!')
-print ('Waiting for clients...')
 
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 
+num_cliente_actual = 1
 #print ('Got connection from', addr)
 while True:
     c, addr = s.accept()     # Establish connection with client.
-    th = threading.Thread(target=on_new_client, args=(c,addr,))
-    th.start()
+    logger_s.info(f' Server: Se ha recibido y aceptado la petición de conexión del cliente {num_cliente_actual}.')
+    th = threading.Thread(target=on_new_client, args=(c,addr, num_cliente_actual))
     
+    logger_s.info(f' Server: Se le ha asignado un hilo al cliente {num_cliente_actual} para el manejo de sus peticiones.')
+    th.start()   
+    num_cliente_actual+=1
     #Note it's (addr,) not (addr) because second parameter is a tuple
     #Edit: (c,addr)
     #that's how you pass arguments to functions when creating new threads using thread module.
